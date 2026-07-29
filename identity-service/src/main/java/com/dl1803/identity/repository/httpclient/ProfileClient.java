8a2620845c50b9ce18d47de2375d722d26fab5e8
dl1803 <loitran0990@gmail.com>
2026-07-29 19:48:36 +0700

    feat: Build, config Api-gateway with Srping Cloud for identity and profile service

--- identity-service/src/main/java/com/dl1803/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/dl1803/identity/repository/httpclient/ProfileClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient (name = "profile-service", url = "${app.services.profile}") // name, root endpoint (phần chung của api profile service)
 public interface ProfileClient {
-    @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE) // produces(RequestMapping) tạo ra 1 request với content-type là Json
+    @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE) // produces(RequestMapping) tạo ra 1 request với content-type là Json
     UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request);
 }
